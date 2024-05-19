a4041bee0c9a6dd7845a832a8c786a3650e9c946
Alex-0202 <alepolon@outlook.com>
2024-05-19 14:30:04 -0300

    atualizando desafio 11 e 12. Acrescentando o 14 e 15

--- 1-Desafios/desafio-11.py
+++ 1-Desafios/desafio-11.py
@@ -8,5 +8,5 @@ area = larg * alt
 area_por_litro = 2
 quantidade_de_tinta = area/area_por_litro
 
-print('A área da parede de {} metro de altura e {} metros de largura é: {} metros quadrados'.format(alt, larg, area))
-print ('A quantidade de tinta necessária é de {:.2f} litros'.format(quantidade_de_tinta))
+print('A sua parece tem a dimenção de {}x{} e sua área é de{}m².'.format(larg, alt, area))
+print('Para pintar essa parece você precisará de {}l de tinta.'.format(quantidade_de_tinta))
--- 1-Desafios/desafio-12.py
+++ 1-Desafios/desafio-12.py
@@ -6,4 +6,5 @@ desconto = preco * 0.05
 
 novo_preco = preco - desconto
 
-print('O valor R${} com 5% de desconto é R${}'.format(preco, novo_preco))
+print('O valor R${} com 5% de desconto é R${:.2f}'.format(preco, novo_preco))
+print('O desconto foi de {:.2f}'.format(desconto))
